70eb165be93242c6f8f5b0e91cbf9cae827c3d17
1noro <ppuubblliicc@protonmail.com>
2021-12-26 14:49:26 +0100

    feat(packages): :sparkles: funcionalidad de obs con wayland gnome e intel

--- packages/install-manual.sh
+++ packages/install-manual.sh
@@ -4,3 +4,17 @@
 # subsituto de yt-dowloander
 sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
 sudo chmod a+rx /usr/local/bin/yt-dlp
+
+
+# obs-stuido in gnome-wayland full featured
+# (https://wiki.archlinux.org/title/Screen_capture#Screencasting)
+sudo pacman -S --noconfirm obs-studio v4l2loopback-dkms && \
+sudo modprobe v4l2loopback exclusive_caps=1 card_label=VirtualVideoDevice && \
+cd /opt/aur && \
+git clone https://aur.archlinux.org/obs-gnome-screencast.git && \
+cd obs-gnome-screencast && \
+makepkg -sri --noconfirm && \
+cd "$HOME" || exit
+# comprobaciónes:
+# v4l2-ctl --list-devices
+# mpv av://v4l2:/dev/video0
